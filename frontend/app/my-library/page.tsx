1f5329c648bdd0ef78245c852c059e1b1769f495
EdwinDij <edwin.d899@gmail.com>
2025-05-23 15:42:58 +0200

    ajout de la page profil et library debut de library en cours

---
+++ frontend/app/my-library/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>c&apos;est ici qu&apos;il y aura la collection de livre</div>
+  )
+}
---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>c&apos;est ici qu&apos;il y aura la page profil pour editer le profil</div>
+  )
+}
